d1714cb8ba3c2cabb539a3cacaa5d87e27f22510
leo-xee <jang.xmin@gmail.com>
2024-08-19 02:35:12 +0900

    feat: AppBar에서 이동 가능한 경로의 페이지 추가
    resolves #13
    
    Signed-off-by: leo-xee <jang.xmin@gmail.com>

---
+++ src/app/books/page.tsx
@@ -0,0 +1,11 @@
+import { Container } from '@/components/common/Container';
+
+export default function Books() {
+  return (
+    <main>
+      <Container>
+        <div>Books</div>
+      </Container>
+    </main>
+  );
+}
