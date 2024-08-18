31852b4f389b4917582ff993098d0267c03dc7d7
leo-xee <jang.xmin@gmail.com>
2024-08-19 02:35:12 +0900

    feat: AppBar에서 이동 가능한 경로의 페이지 추가
    resolves #13
    
    Signed-off-by: leo-xee <jang.xmin@gmail.com>

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,11 @@
+import { Container } from '@/components/common/Container';
+
+export default function Posts() {
+  return (
+    <main>
+      <Container>
+        <div>Posts</div>
+      </Container>
+    </main>
+  );
+}
---
+++ src/app/problem-solving/page.tsx
@@ -0,0 +1,11 @@
+import { Container } from '@/components/common/Container';
+
+export default function Problems() {
+  return (
+    <main>
+      <Container>
+        <div>Problems</div>
+      </Container>
+    </main>
+  );
+}
