96482df908d818295dd9a436810033b45b83e362
leo-xee <jang.xmin@gmail.com>
2024-09-01 01:38:56 +0900

    feat: 폰트 사이즈 반응형 대응
    - 모바일 해상도에서는 heading 폰트 사이즈 작게 노출되도록 함
    
    resolves #42
    
    Signed-off-by: leo-xee <jang.xmin@gmail.com>

--- src/components/PostList/PostList.css.ts
+++ src/components/PostList/PostList.css.ts
@@ -1,14 +1,8 @@
 import { vars } from '@/styles/themes.css';
 import { style } from '@vanilla-extract/css';
-import { calc } from '@vanilla-extract/css-utils';
+import { heading2, heading3 } from '../common/Markdown/Markdown.css';
 
-export const title = style({
-  paddingTop: vars.space.space12,
-  paddingBottom: vars.space.space04,
-  fontSize: calc.add(vars.fontSize.heading2, '4px'),
-  fontWeight: vars.fontWeight.bold,
-  color: vars.themeColor.heading,
-});
+export const title = style([heading2, {}]);
 
 export const postList = style({
   display: 'flex',
@@ -19,17 +13,14 @@ export const postList = style({
 export const postListItem = style({
   display: 'flex',
   flexDirection: 'column',
-  gap: vars.space.space02,
+  gap: vars.space.space01,
   width: '100%',
   padding: vars.space.space02,
 });
 
-export const postTitle = style({
-  fontSize: calc.subtract(vars.fontSize.heading3, '2px'),
-  fontWeight: vars.fontWeight.bold,
-  color: vars.themeColor.heading,
-});
-
-export const postDescription = style({
-  fontSize: '15px',
-});
+export const postTitle = style([
+  heading3,
+  {
+    margin: vars.space.none,
+  },
+]);
