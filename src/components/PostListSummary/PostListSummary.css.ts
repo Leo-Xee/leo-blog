2f250a48d5cd0531700f7e2259ac2716a279a098
leo-xee <jang.xmin@gmail.com>
2024-09-03 23:32:28 +0900

    feat: Home 페이지 구현 및 페이지별  문구 추가
    - Home, Tab별 페이지에 가치관 소개에 대한 문구를 추가함
    - Home에 cursor에 반응하는 UI를 추가함
    
    resolves #22
    
    Signed-off-by: leo-xee <jang.xmin@gmail.com>

--- src/components/PostListSummary/PostListSummary.css.ts
+++ src/components/PostListSummary/PostListSummary.css.ts
@@ -5,6 +5,7 @@ import { heading1 } from '../common/Markdown/Markdown.css';
 export const title = style([heading1, {}]);
 
 export const description = style({
+  paddingTop: vars.space.space03,
   paddingLeft: vars.space.space02,
   paddingRight: vars.space.space02,
 });
