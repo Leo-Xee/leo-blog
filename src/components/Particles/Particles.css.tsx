689472fe592a3f2cc2784856be960cf169ae5e93
leo-xee <jang.xmin@gmail.com>
2024-09-03 23:11:37 +0900

    feat: Home 페이지 구현 및 페이지별  문구 추가
    - Home, Tab별 페이지에 가치관 소개에 대한 문구를 추가함
    - Home에 cursor에 반응하는 UI를 추가함
    
    resolves #22
    
    Signed-off-by: leo-xee <jang.xmin@gmail.com>

---
+++ src/components/Particles/Particles.css.tsx
@@ -0,0 +1,106 @@
+import { vars } from '@/styles/themes.css';
+import { style } from '@vanilla-extract/css';
+
+export const container = style({
+  position: 'absolute',
+  top: '-20px',
+  width: '100%',
+  height: '300px',
+  willChange: 'transform, opacity',
+});
+
+export const particle1 = style({
+  position: 'absolute',
+  top: '80px',
+  left: '18%',
+  width: '12px',
+  height: '12px',
+  opacity: 0.4,
+  backgroundColor: '#55a5ff',
+  borderRadius: vars.borderRadius.rounded,
+  willChange: 'transform, opacity',
+});
+
+export const particle2 = style({
+  position: 'absolute',
+  top: '45px',
+  left: '36%',
+  width: '16px',
+  height: '16px',
+  opacity: 0.2,
+  backgroundColor: '#f4717d',
+  borderRadius: vars.borderRadius.rounded,
+  willChange: 'transform, opacity',
+});
+
+export const particle3 = style({
+  position: 'absolute',
+  top: '50px',
+  right: '31%',
+  width: '24px',
+  height: '24px',
+  opacity: 0.4,
+  backgroundColor: '#8474f9',
+  borderRadius: vars.borderRadius.rounded,
+  willChange: 'transform, opacity',
+});
+
+export const particle4 = style({
+  position: 'absolute',
+  top: '90px',
+  right: '20%',
+  width: '12px',
+  height: '12px',
+  opacity: 0.15,
+  backgroundColor: '#f4717d',
+  borderRadius: vars.borderRadius.rounded,
+  willChange: 'transform, opacity',
+});
+
+export const particle5 = style({
+  position: 'absolute',
+  bottom: '120px',
+  left: '23%',
+  width: '24px',
+  height: '24px',
+  opacity: 0.3,
+  backgroundColor: '#f4717d',
+  borderRadius: vars.borderRadius.rounded,
+  willChange: 'transform, opacity',
+});
+
+export const particle6 = style({
+  position: 'absolute',
+  bottom: '100px',
+  left: '36%',
+  width: '16px',
+  height: '16px',
+  opacity: 0.2,
+  backgroundColor: '#8474f9',
+  borderRadius: vars.borderRadius.rounded,
+  willChange: 'transform, opacity',
+});
+
+export const particle7 = style({
+  position: 'absolute',
+  bottom: '80px',
+  right: '35%',
+  width: '16px',
+  height: '16px',
+  opacity: 0.2,
+  backgroundColor: '#55a5ff',
+  borderRadius: vars.borderRadius.rounded,
+  willChange: 'transform, opacity',
+});
+
+export const particle8 = style({
+  position: 'absolute',
+  bottom: '120px',
+  right: '23%',
+  width: '12px',
+  height: '12px',
+  opacity: 0.2,
+  backgroundColor: '#8474f9',
+  borderRadius: vars.borderRadius.rounded,
+  willChange: 'transform, opacity',
+});
