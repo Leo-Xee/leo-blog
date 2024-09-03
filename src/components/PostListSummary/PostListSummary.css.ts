4d2a82b20a2c022243dab40792b97601b740355f
Jangmin Lee <jang.xmin@gmail.com>
2024-09-03 23:24:47 +0900

    Revert "feat: Home 페이지 구현 및 페이지별  문구 추가"
    This reverts commit 52f7d31020611fc87e2d4e219a270d008bbfaf4b.

--- src/components/PostListSummary/PostListSummary.css.ts
+++ src/components/PostListSummary/PostListSummary.css.ts
@@ -5,7 +5,6 @@ import { heading1 } from '../common/Markdown/Markdown.css';
 export const title = style([heading1, {}]);
 
 export const description = style({
-  paddingTop: vars.space.space03,
   paddingLeft: vars.space.space02,
   paddingRight: vars.space.space02,
 });
