abf8ff34e7e4f95e2136998f68b4eb7058614f73
leo-xee <jang.xmin@gmail.com>
2024-09-08 16:36:50 +0900

    refactor: 페이지 내 고정 문구 상수로 분리
    - meta 태그에서 문구를 사용하기 위해 Home과 Tab별로 고정된 문구를 상수로 분리함
    
    resolves #16
    
    Signed-off-by: leo-xee <jang.xmin@gmail.com>

--- src/components/PostListSummary/PostListSummary.tsx
+++ src/components/PostListSummary/PostListSummary.tsx
@@ -1,12 +1,7 @@
 import { Post, PostType } from '@/types/post';
 import React from 'react';
+import { POST_LIST_SUMMARY } from '@/constants';
 import * as styles from './PostListSummary.css';
-
-type Summary = {
-  type: PostType;
-  title: string;
-  description: string;
-};
 
 type PostListSummaryProps = {
   type: PostType;
@@ -14,31 +9,31 @@ type PostListSummaryProps = {
 };
 
 function PostListSummary({ posts, type }: PostListSummaryProps) {
-  const summaries: Summary[] = [
+  const summariesStatus = [
     {
       type: 'articles',
-      title: 'Articles',
-      description: `하나의 기록이 점이 되고, 그 점들이 이어져 선이 되어서 결국 나아갈 방향을 이끌어준다고 믿어요.
-                    학습한 내용, 겪은 시행착오, 그리고 생각의 흐름을 정리하며 이를 통해 더 나은 길을 찾아가고자 해요.
-                    지금까지 총 ${posts.length}개의 글을 작성했어요.`,
+      description: `지금까지 총 ${posts.length}개의 글을 작성했어요.`,
     },
     {
       type: 'problem-solving',
-      title: 'Problem Solving',
-      description: `기술은 빠르게 변화하지만, 이를 근본적으로 지탱하고 있는 것은 자료구조와 알고리즘이라고 믿어요.
-                    주로 백준 온라인 저지, LeetCode의 알고리즘 문제를 C++와 JavaScript 언어를 기반으로 꾸준히 풀고 있어요.
-                    이를 통해 더 복잡한 문제에도 자신감을 가지고 도전할 수 있도록 노력하고 있어요.
-                    지금까지 총 ${posts.length}개의 알고리즘 문제를 풀고 글을 작성했어요.`,
+      description: `지금까지 총 ${posts.length}개의 알고리즘 문제를 풀고 글을 작성했어요.`,
     },
     {
       type: 'books',
-      title: 'Books',
-      description: `요량이 아닌 본질이 담긴 깊이 있는 지식은 단편적인 글이 아닌 책 속에 있다고 믿어요.
-                    특히 기술, 디자인, 인문학, 경제 분야에 관심이 많아서 조금씩이라도 꾸준히 책을 읽으며 지식을 넓혀가고자 해요.
-                    이 과정을 통해 다양한 관점을 얻고, 더 깊이 있는 이해를 쌓아가고 있어요.
-                    지금까지 총 ${posts.length}권의 책을 읽고 글을 작성했어요.`,
+      description: `지금까지 총 ${posts.length}권의 책을 읽고 글을 작성했어요.`,
     },
   ];
+
+  const summaries = POST_LIST_SUMMARY.map((summary) => {
+    const status = summariesStatus.find(
+      (currentStatus) => currentStatus.type === summary.type
+    );
+
+    return {
+      ...summary,
+      description: `${summary.description} ${status?.description ?? ''}`,
+    };
+  });
 
   const currentSummary = summaries.find((summary) => summary.type === type);
 
