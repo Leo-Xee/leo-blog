d0e6d9ed98fa312d471530fa743127be254c602f
leo-xee <jang.xmin@gmail.com>
2025-04-20 23:43:13 +0900

    chore: CI/CD 자동화 반영
    - AWS S3, CloudFront 기반 CI/CD 자동화를 적용함
    
    resolves #92
    
    Signed-off-by: leo-xee <jang.xmin@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,17 @@ import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
 
 const withVanillaExtract = createVanillaExtractPlugin();
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    loader: 'custom',
+    loaderFile: './image-loader.ts',
+  },
+  assetPrefix: isProduction ? 'https://leo-xee.dev' : '',
+};
 
 export default withVanillaExtract(nextConfig);
